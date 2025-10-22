86becb456189caca35caabe646489e80a760fba3
Pratyush Prasar <69028293+pratp7@users.noreply.github.com>
2025-10-22 14:42:12 +0000

    adding the supergateway script

--- start.sh
+++
@@ -1,6 +0,0 @@
-npx -y supergateway \
-  --stdio "npx -y chrome-devtools-mcp@latest --headless=true" \
-  --outputTransport streamableHttp \
-  --stateful \
-  --sessionTimeout 60000 \
-  --port ${PORT:-8000}
